8c62ab1d29cc3808dec2227a46a62bee20dfe7bb
ritsudo <minato-namikaje@yandex.ru>
2024-02-05 10:43:01 +0300

    orange pi pc version

---
+++ example.c
@@ -0,0 +1,20 @@
+#include "LPH91572.h"
+#include <wiringPi.h>
+
+int main (void)
+{
+	wiringPiSetup();
+	LCD_init();
+	
+	for (;;)
+	{
+		LCD_FillScreen (0xFFFF);
+		LCD_Puts_Shadow("Hello", 6, 20, 0x0000,  4, 4, 90);
+		LCD_Puts_Shadow("Hello", 4, 18, 0xFFFF,  4, 4, 90);
+		LCD_Puts_Shadow("Hello", 4, 60, 0xFFFF,  2, 2, 90);
+		LCD_Puts_Shadow("Hello", 4, 90, 0x0000,  1, 1, 90);
+		delay(2000);
+	}
+	return 0;
+}
+
