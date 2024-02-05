fe3ee386215bc2b6bb13959c92628b57979eb726
ritsudo <minato-namikaje@yandex.ru>
2024-02-05 20:32:54 +0300

    try Hoshi's code 1

--- example.c
+++ example.c
@@ -2,24 +2,101 @@
 #include <stdlib.h>
 #include "LPH91572.h"
 
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SCREEN_WIDTH 132
+#define SCREEN_HEIGHT 176
+
 int main (void)
 {
 	LCD_init();
 
-	unsigned short myScreenShot[23232];
 	
 	//Забивка скриншота 132x176
 	
+	/*
+	
 	FILE* f_scr = fopen("test.bmp", "r");
 	fseek(f_scr, 0x46, SEEK_SET); // skip bmp header
-	fread(&myScreenShot, 1, 176 * 132 * 2/*16bit*/, f_scr);
+	fread(&myScreenShot, 1, 176 * 132 * 2, f_scr); // 2 means 16 bit, USE R5G6B5 palette
 	fclose(f_scr);
 	
 	for (int x = 0; x < 23232; x += 1) {
 		myScreenShot[x] = (myScreenShot[x] >> 8 | myScreenShot[x] << 8);
 	}
 	
-	LCD_FillScreen(myScreenShot);
+	*/
+	
+	uint16_t screen[SCREEN_HEIGHT][SCREEN_WIDTH];
+    uint16_t old_fb[FB_HEIGHT * FB_WIDTH];
+
+    int fd_scr = open("/dev/fb0", O_RDONLY);
+    int scr_sz = FB_HEIGHT * FB_WIDTH * 2/*16bit*/;
+    uint16_t* fb_screenshot = mmap(0, scr_sz, PROT_READ, MAP_PRIVATE, fd_scr, 0);
+
+    // scaling
+    float scale_X = FB_HEIGHT / (float)SCREEN_WIDTH;
+    float scale_Y = FB_WIDTH / (float)SCREEN_HEIGHT;
+
+    int frame_cnt = 0;
+    struct timespec ts1, ts2;
+    clock_gettime(CLOCK_MONOTONIC, &ts1);
+
+    for(;;) // forever
+    {
+        if(dynamic_fps)
+            if(memcmp(&old_fb, fb_screenshot, sizeof(old_fb)) == 0)
+            {
+                usleep(10000);
+                continue;
+            }
+            else
+            {
+                memcpy(&old_fb, fb_screenshot, sizeof(old_fb));
+            }
+
+        for(int x = 0; x < SCREEN_WIDTH; x++)
+            for(int y = 0; y < SCREEN_HEIGHT; y++)
+            {
+                int fb_x = y * scale_X;
+                int fb_y = x * scale_Y;
+
+                uint16_t px = fb_screenshot[fb_x + fb_y * FB_WIDTH];
+
+                screen[y][SCREEN_WIDTH - 1 - x] = (px << 8) | (px >> 8);
+            }
+
+        LCD_FillScreen((char*)&screen[0][0]);
+
+        /// calc fps
+        frame_cnt++;
+        if(frame_cnt >= 100)
+        {
+            clock_gettime(CLOCK_MONOTONIC, &ts2);
+
+            float allsec = (ts2.tv_sec - ts1.tv_sec) + (ts2.tv_nsec - ts1.tv_nsec) / 1000000000.0;
+            float fps = frame_cnt / allsec;
+
+            printf("%f FPS\n", fps);
+
+            frame_cnt = 0;
+            clock_gettime(CLOCK_MONOTONIC, &ts1);
+        }
+
+        usleep(1000);
+    }
+
+    munmap(fb_screenshot, scr_sz);
+    close(fd_scr);
+    close_gpio();
+
+    printf("fin\n"); 
 	
 	return 0;
 }
