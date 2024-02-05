53a3cbc5ca7277e831928e25bdef0c9dc400ceaa
ritsudo <minato-namikaje@yandex.ru>
2024-02-05 20:04:03 +0300

    trobleshoot 2

--- example.c
+++ example.c
@@ -11,7 +11,7 @@ int main (void)
 	//Забивка скриншота 132x176
 	
 	FILE* f_scr = fopen("test2.bmp", "r");
-	fseek(f_scr, 0x42, SEEK_SET); // skip bmp header
+	fseek(f_scr, 0x36, SEEK_SET); // skip bmp header
 	fread(&myScreenShot, 1, 176 * 132 * 2/*16bit*/, f_scr);
 	fclose(f_scr);
 	
