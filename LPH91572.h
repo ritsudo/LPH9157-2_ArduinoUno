0278be7c3364c64c3272a94a25ec9917d5d2453b
ritsudo <minato-namikaje@yandex.ru>
2024-02-05 16:12:47 +0300

    trying refactor library 14

--- LPH91572.h
+++ LPH91572.h
@@ -109,11 +109,13 @@ void paint ()
 
 	for (unsigned char i = 0; i < 11; i += 1)
 	{
-		memcpy(mySendBuffer, (myScreenBuffer), 4096);	
+		unsigned short *myPointer = &myScreenBuffer[(i*4096)];
+		memcpy(mySendBuffer, myPointer, 4096);	
 		wiringPiSPIDataRW(0, dataPointer, 4096);
 	}
-	
-	memcpy(mySendBuffer, (myScreenBuffer), 1408);	
+
+	unsigned short *myPointer = &myScreenBuffer[(11*4096)];	
+	memcpy(mySendBuffer, myPointer, 1408);	
 	wiringPiSPIDataRW(0, dataPointer, 1408);
 
 	
