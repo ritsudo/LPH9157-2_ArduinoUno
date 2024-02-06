d668a35c5f8cc864d2e12af11ff47cfa5d70b70b
ritsudo <minato-namikaje@yandex.ru>
2024-02-06 19:33:32 +0300

    trying to convert color 11

--- example.c
+++ example.c
@@ -39,9 +39,11 @@ int main (void)
 		{
 			cell = bufferEnd - (y*SCREEN_WIDTH + (SCREEN_WIDTH-x-1))*3;
 			
-//			uint16_t newColorByte = (inputScreen[cell] & 0b11111000); //red part
-//			uint16_t newColorByte = ((inputScreen[cell+1] & 0b11100000) >> 5) | ((inputScreen[cell+1] & 0b00011100) << 11); //green part
-			uint16_t newColorByte = ((inputScreen[cell+2] & 0b11111000) << 5); //blue part
+			uint16_t newColorByte = 
+			  (inputScreen[cell] & 0b11111000)
+			| ((inputScreen[cell+1] & 0b11100000) >> 5) | ((inputScreen[cell+1] & 0b00011100) << 11)
+			| ((inputScreen[cell+2] & 0b11111000) << 5);
+			
 			screen[y][x] = newColorByte;
 		}
 	}
