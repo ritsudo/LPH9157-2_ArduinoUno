9df988d93c7521bd5c983d35e0f12ea333b1be93
ritsudo <minato-namikaje@yandex.ru>
2024-02-06 19:20:48 +0300

    trying to convert color 8

--- example.c
+++ example.c
@@ -40,7 +40,7 @@ int main (void)
 			cell = bufferEnd - (y*SCREEN_WIDTH + (SCREEN_WIDTH-x-1))*3;
 			
 //			uint16_t newColorByte = (inputScreen[cell] & 0b11111000); //red part
-			uint16_t newColorByte = ((inputScreen[cell+2] & 0b11111100) << 5); //green part
+			uint16_t newColorByte = ((inputScreen[cell+1] & 0b11111100) << 3); //green part
 //			uint16_t newColorByte = ((inputScreen[cell+2] & 0b11111000) << 11); //blue part
 			screen[y][x] = newColorByte;
 		}
